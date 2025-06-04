27118448cd821dc08eaabdd015341a97c00a8db0
Lena <elena.mikhaylova0@gmail.com>
2025-06-04 10:30:08 -0400

    update api file

--- app/lib/swagger.ts
+++ app/lib/swagger.ts
@@ -3,6 +3,7 @@ import { createSwaggerSpec } from "next-swagger-doc";
 export const getApiDocs = async () => {
   const spec = createSwaggerSpec({
     apiFolder: "app/(api)/v1/", // Updated to point to the actual API routes directory
+    apis: [`${__dirname}/routes/*.ts`],
     definition: {
       openapi: "3.0.0",
       info: {
