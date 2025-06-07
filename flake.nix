dde56351b9b16e3fbc9c85a57a29bc64d59fadbd
Parthiv Seetharaman <parthivs@myrdd.info>
2025-06-06 23:48:52 -0700

    feat: add nixosModule to flake

--- flake.nix
+++ flake.nix
@@ -6,40 +6,63 @@
     gomod2nix.url = "github:nix-community/gomod2nix";
     devshell.url = "github:numtide/devshell";
   };
-  outputs = inputs @ {
-    self,
-    flake-parts,
-    ...
-  }: flake-parts.lib.mkFlake { inherit inputs; } {
-    imports = [
-      inputs.devshell.flakeModule
-    ];
-    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
-    perSystem = {
-      pkgs,
-      system,
-      config,
-      ...
-    }: {
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [
-          inputs.gomod2nix.overlays.default
-        ];
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ moduleWithSystem, ... }: {
+      imports = [ inputs.devshell.flakeModule ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      perSystem = { pkgs, system, config, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.gomod2nix.overlays.default ];
+        };
+        packages.default = config.packages.indexer;
+        packages.vault-indexer = pkgs.buildGoApplication {
+          pname = "vault-indexer";
+          version = "latest";
+          src = ./.;
+          modules = ./gomod2nix.toml;
+          doCheck = false;
+        };
+        devshells.default = {
+          commands =
+            [ { package = pkgs.gomod2nix; } { package = pkgs.nixfmt; } ];
+        };
       };
-      packages.default = config.packages.indexer;
-      packages.indexer = pkgs.buildGoApplication {
-        pname = "vault-indexer";
-        version = "latest";
-        src = ./.;
-        modules = ./gomod2nix.toml;
-        doCheck = false;
-      };
-      devshells.default = {
-        commands = [
-          {package = pkgs.gomod2nix;}
-        ];
-      };
-    };
-  };
+      flake.nixosModules.vault-indexer = moduleWithSystem ({ self' }:
+        { lib, config, ... }:
+        let
+          cfg = config.services.vault-indexer;
+        in
+        {
+          options = {
+            services.vault-indexer = {
+              enable = lib.mkEnableOption "Vault Indexer";
+            };
+
+          };
+          config = lib.mkIf cfg.enable {
+            systemd.services.vault-indexer = {
+              description = "vault indexer";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              environment = {
+                ENV = "prod";
+              };
+              preStart = ''
+                mkdir -p github.com/timewave/
+                if [[ -d github.com/timewave/vault-indexer ]]; then
+                  unlink github.com/timewave/vault-indexer
+                fi
+                ln -s ${self'.packages.vault-indexer.src} github.com/timewave/vault-indexer
+                cp -r ${self'.packages.vault-indexer.src}/abis .
+              '';
+              serviceConfig = {
+                ExecStart = "${lib.getExe self'.packages.vault-indexer} prod";
+                StateDirectory = "vault-indexer";
+                WorkingDirectory = "/var/lib/vault-indexer";
+              };
+            };
+          };
+        });
+    });
 }
