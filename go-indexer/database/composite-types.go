600667df93643a1566d141fdd215f5bc2e5b1e3e
Lena <elena.mikhaylova0@gmail.com>
2025-06-13 16:19:18 -0400

    fix empty string

--- go-indexer/database/composite-types.go
+++ go-indexer/database/composite-types.go
@@ -1,10 +1,10 @@
 package database
 
 type PositionUpdate struct {
-	Id                      string `json:"id"`
-	IsTerminated            bool   `json:"is_terminated"`
-	PositionEndHeight       int64  `json:"position_end_height"`
-	WithdrawReceiverAddress string `json:"withdraw_receiver_address"`
+	Id                      string  `json:"id"`
+	IsTerminated            bool    `json:"is_terminated"`
+	PositionEndHeight       int64   `json:"position_end_height"`
+	WithdrawReceiverAddress *string `json:"withdraw_receiver_address"`
 }
 
 func ToPositionUpdate(u PublicPositionsUpdate) PositionUpdate {
@@ -13,24 +13,26 @@ func ToPositionUpdate(u PublicPositionsUpdate) PositionUpdate {
 		Id:                      *u.Id,
 		IsTerminated:            *u.IsTerminated,
 		PositionEndHeight:       *u.PositionEndHeight,
-		WithdrawReceiverAddress: *u.WithdrawReceiverAddress,
+		WithdrawReceiverAddress: u.WithdrawReceiverAddress,
 	}
 }
 
 type PositionInsert struct {
-	PositionIndexId     int64  `json:"position_index_id"`
-	OwnerAddress        string `json:"owner_address"`
-	ContractAddress     string `json:"contract_address"`
-	AmountShares        string `json:"amount_shares"`
-	PositionStartHeight int64  `json:"position_start_height"`
+	PositionIndexId         int64   `json:"position_index_id"`
+	OwnerAddress            string  `json:"owner_address"`
+	ContractAddress         string  `json:"contract_address"`
+	AmountShares            string  `json:"amount_shares"`
+	PositionStartHeight     int64   `json:"position_start_height"`
+	WithdrawReceiverAddress *string `json:"withdraw_receiver_address"`
 }
 
 func ToPositionInsert(u PublicPositionsInsert) PositionInsert {
 	return PositionInsert{
-		PositionIndexId:     u.PositionIndexId,
-		OwnerAddress:        u.OwnerAddress,
-		ContractAddress:     u.ContractAddress,
-		AmountShares:        u.AmountShares,
-		PositionStartHeight: u.PositionStartHeight,
+		PositionIndexId:         u.PositionIndexId,
+		OwnerAddress:            u.OwnerAddress,
+		ContractAddress:         u.ContractAddress,
+		AmountShares:            u.AmountShares,
+		PositionStartHeight:     u.PositionStartHeight,
+		WithdrawReceiverAddress: nil,
 	}
 }
