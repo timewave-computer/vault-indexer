21085ef519028115f9fc078c5389fd470de31682
Lena <elena.mikhaylova0@gmail.com>
2025-07-09 17:24:25 -0400

    add reorg logger

--- go-indexer/indexer/reorg-handler.go
+++ go-indexer/indexer/reorg-handler.go
@@ -7,40 +7,42 @@ import (
 
 	"github.com/supabase-community/postgrest-go"
 	"github.com/timewave/vault-indexer/go-indexer/database"
+	"github.com/timewave/vault-indexer/go-indexer/logger"
 )
 
 func (i *Indexer) handleReorg(reorgEvent ReorgEvent) {
-	i.logger.Info("Handling reorg event: %+v", reorgEvent)
+	logger := logger.NewLogger("ReorgHandler")
+	logger.Info("Handling reorg event: %+v", reorgEvent)
 
 	// 1. Stop all child processes using shared halt manager
-	i.logger.Info("Halting all child processes due to reorg...")
+	logger.Info("Halting all child processes due to reorg...")
 	i.haltManager.Halt()
 
 	// Wait for all processors to halt
 	isHalted := i.haltManager.WaitHalted(120 * time.Second)
 	if !isHalted {
-		i.logger.Error("Processors did not halt in time")
+		logger.Error("Processors did not halt in time")
 		i.Stop()
 		return
 	}
-	i.logger.Info("All processors halted")
+	logger.Info("All processors halted")
 
 	// 2. Clear the event queue
-	i.logger.Info("Clearing event queue due to reorg...")
+	logger.Info("Clearing event queue due to reorg...")
 	i.eventQueue.Clear()
 
 	// 3. Stop the event processor
-	i.logger.Info("Stopping event processor due to reorg...")
+	logger.Info("Stopping event processor due to reorg...")
 
 	// 3. Execute cleanup logic
-	i.logger.Info("Finding last consistent block before block number: %d", reorgEvent.BlockNumber)
-	lastConsistentBlock, err := i.findLastConsistentBlock(reorgEvent.BlockNumber)
+	logger.Info("Finding last consistent block before block number: %d", reorgEvent.BlockNumber)
+	lastConsistentBlock, err := i.findLastConsistentBlock(reorgEvent.BlockNumber, logger)
 	if err != nil {
 		i.logger.Error("Failed to find last consistent block: %v", err)
 		i.Stop()
 		return
 	}
-	i.logger.Info("Executing cleanup logic after last consistent block number: %d", lastConsistentBlock)
+	logger.Info("Executing cleanup logic after last consistent block number: %d", lastConsistentBlock)
 	err = i.transformer.handleCleanupFromBlock(lastConsistentBlock, i.postgresClient)
 	if err != nil {
 		i.logger.Error("Failed to cleanup reorg data: %v", err)
@@ -56,8 +58,8 @@ func (i *Indexer) handleReorg(reorgEvent ReorgEvent) {
 }
 
 // findLastConsistentBlock finds the last block that is still valid by checking the canonical chain
-func (i *Indexer) findLastConsistentBlock(reorgDetectedBlock int64) (int64, error) {
-	i.logger.Debug("Searching for last valid block before %d", reorgDetectedBlock)
+func (i *Indexer) findLastConsistentBlock(reorgDetectedBlock int64, logger *logger.Logger) (int64, error) {
+	logger.Debug("Searching for last valid block before %d", reorgDetectedBlock)
 
 	fromBlock := reorgDetectedBlock - 1
 	for {
@@ -70,7 +72,7 @@ func (i *Indexer) findLastConsistentBlock(reorgDetectedBlock int64) (int64, erro
 		}
 
 		isCanonical, err := CheckCanonicalBlock(i.ethClient, i.logger, nearestIngestedEvent.BlockNumber, nearestIngestedEvent.BlockHash)
-		i.logger.Debug("Is canonical: %v, block number: %v, hash: %v", isCanonical, nearestIngestedEvent.BlockNumber, nearestIngestedEvent.BlockHash)
+		logger.Debug("Is canonical: %v, block number: %v, hash: %v", isCanonical, nearestIngestedEvent.BlockNumber, nearestIngestedEvent.BlockHash)
 		if err != nil {
 			return 0, err
 		}
