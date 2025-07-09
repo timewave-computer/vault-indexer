a312f6f1018a78e99603120b95d24854f125e521
Lena <elena.mikhaylova0@gmail.com>
2025-07-09 17:50:13 -0400

    rm console log

--- app/lib/query-util.ts
+++ app/lib/query-util.ts
@@ -31,7 +31,6 @@ const _fetchLastValidatedBlockNumber = async (
  
     try {
         const {data, error} = await supabase.from('block_finality').select('last_validated_block_number').eq('block_tag', blockTag)
-        console.log('data',data)
         if (error) {
             throw new Error('Could not fetch finalized block', { cause: error as Error });
         }
